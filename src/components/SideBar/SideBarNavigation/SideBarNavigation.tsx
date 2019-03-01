25b40259a6170c5ca221d6375d481f0b7eda4242
Maxim_Ozarovskiy <maxim.ozarovskiy@gmail.com>
2019-03-01 23:34:58 +0200

    need to be fix SideBarNav

--- src/components/SideBar/SideBarNavigation/SideBarNavigation.tsx
+++ src/components/SideBar/SideBarNavigation/SideBarNavigation.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import classNames from 'classnames';
 
-import image from '../../../icons/creative.png';
+import image from '../../../icons/creative.svg';
 import styles from './SideBarNavigation.module.scss';
 import { Props } from './types';
 
 export class SideBarNavigation extends React.Component<Props> {
+  openOrNot = () => {
+    if (this.props.isOpen){
+      return classNames(styles.open);
+    } else {
+      return classNames(styles.close);
+    }
+  };
   getItemClassName = () => {
-    return classNames('items', { open_items: this.props.isOpen });
+    if (this.props.isOpen){
+      return classNames(styles.open_items);
+    } else {
+      return classNames(styles.items);
+    }
   };
 
   render() {
     return (
       <div className={styles.container}>
-        <div>
+        <div className={this.openOrNot()}>
           <div className={this.getItemClassName()}>
             <img src={image} />
             <a> Some Text </a>
@@ -23,7 +34,13 @@ export class SideBarNavigation extends React.Component<Props> {
             <a> Some Other Text </a>
           </div>
         </div>
-        <div>auth</div>
+       {/*user auth part*/}
+        <div className={this.openOrNot()}>
+          <div className={this.getItemClassName()}>
+            <img src={image} />
+            <a> Login </a>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,7 +4,13 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
 import { Props } from './types';
+import user_image from '../../icons/user.svg'
+import './styles.css';
 
 export class Header extends React.Component<Props> {
   render() {
@@ -14,6 +20,20 @@ export class Header extends React.Component<Props> {
           <IconButton color="inherit" aria-label="Menu" onClick={this.props.onBurgerClick}>
             <MenuIcon />
           </IconButton>
+          <Typography variant="h6" color="inherit">
+            FEROTEKH
+          </Typography>
+          <h1 className="style">   </h1>
+          <img className="image" src={user_image}/>
+          <IconButton
+            aria-label="More"
+            aria-owns={open ? 'long-menu' : undefined}
+            aria-haspopup="true"
+
+          >
+            <MoreVertIcon />
+          </IconButton>
+          <Button color="inherit">SOME</Button>
         </Toolbar>
       </AppBar>
     );
