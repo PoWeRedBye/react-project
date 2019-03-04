83860283eeb15da7379eee45bf2bace1c5e933aa
Maxim_Ozarovskiy <maxim.ozarovskiy@gmail.com>
2019-03-04 22:43:39 +0200

    small fix

---
+++ src/components/material_mini_drawer/MaterialDrawer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import classNames from 'classnames';
+
+
+export class MaterialDrawer extends React.Component {
+
+
+
+  //RENDER
+  render() {
+    return(
+      <div>a</div>
+    );
+  }
+}
--- src/pages/UserProfilePage/index.tsx
+++ src/pages/UserProfilePage/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import photo_background from '../../icons/sr_558908_large.jpg'
-import user_photo from '../../icons/user.svg'
-import styles from './UserProfilePage.module.scss'
+import settings from '../../icons/help.svg'
+import user_photo from '../../icons/user.svg';
+import styles from './UserProfilePage.module.scss';
+import {NavLink} from "react-router-dom";
 
 
 export class UserProfilePage extends React.Component {
@@ -11,21 +12,37 @@ export class UserProfilePage extends React.Component {
   render() {
     return (
       <div className={styles.profile_top}>
-        <img className={styles.img_fluid} alt="profile banner" src={photo_background} height={200}/>
-        <div className={styles.profile_content}>
+        {/*<img className={styles.img_fluid} alt="profile banner" src={photo_background} height={200}/>*/}
+        <div className={styles.profile_info}>
           <div className={styles.media}>
             <img className={styles.rounded_circle} alt="user photo" src={user_photo} width={140} height={140}/>
             <div className={styles.media_body}>
               <div className={styles.mb_20}>
-                <h2>user name</h2>
+                <h2 className={styles.user_name}>user name</h2>
                 <p>user@email.com</p>
               </div>
             </div>
           </div>
         </div>
-        <div>
-          <h1>user name</h1>
-          <h1>user mail</h1>
+        <div className={styles.profile_content}>
+          <div className={styles.profile_content_left}>
+            <h1>user position:</h1>
+            <h2>Some Position</h2>
+            <h1>user phone:</h1>
+            <h2>+38(???)??-??-???</h2>
+            <h2>+38(???)??-??-???</h2>
+          </div>
+          <div className={styles.profile_content_right}>
+            <div className={styles.profile_content_right_header}>
+              <img className={styles.profile_content_right_header_img} src={settings} width={50} height={50}/>
+              <h2 className={styles.profile_content_right_h2}>Settings</h2>
+            </div>
+            <div className={styles.profile_content_right}>
+              <h2 className={styles["component-btn"]}>Change Password</h2>
+              <h2 className={styles["component-btn"]}>Change Email</h2>
+              <h2 className={styles["component-btn"]}>Change User Information</h2>
+            </div>
+          </div>
         </div>
       </div>
     );
