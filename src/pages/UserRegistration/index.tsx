ffbe5f818916bb1997e0ca920148731013f7d554
Maxim_Ozarovskiy <maxim.ozarovskiy@gmail.com>
2019-03-07 21:15:57 +0200

    small fix

---
+++ src/pages/UserRegistration/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './UserRegistration.module.scss'
+
+export class UserRegistration extends React.Component{
+
+
+  //RENDER
+  render() {
+    return(
+      <div className={styles.user_register}>
+        <div className={styles.user}>
+
+        </div>
+      </div>
+    );
+  }
+}
