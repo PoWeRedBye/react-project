c64dbcb0136bd4863f21d99441b2fc3a54d11d37
Maxim_Ozarovskiy <maxim.ozarovskiy@gmail.com>
2019-03-10 22:39:28 +0200

    header styles fix part 1

--- src/components/material_mini_header/MaterialHeader.tsx
+++ src/components/material_mini_header/MaterialHeader.tsx
@@ -6,9 +6,10 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-//
+import classnames from 'classnames';
 import { Props } from './types';
-import { withStyles } from "@material-ui/core";
+import { withStyles } from '@material-ui/core';
+import myStyles from './MaterialHeader.module.scss'
 
 const styles = (theme: any) => ({
   root: {
@@ -37,19 +38,50 @@ const styles = (theme: any) => ({
 });
 
 class MaterialHeader extends React.Component<Props> {
-
   //RENDER
   render() {
     const { classes } = this.props;
     return (
       <div>
-        <AppBar position="fixed" className={classes.appBar}>
-          <Toolbar classes={{ gutters: classes.myToolbar }}>
+        <AppBar
+          position="fixed"
+          className={classnames(
+            'MuiPaper-root-15',
+            'MuiPaper-elevation4-21',
+            'MuiAppBar-root-6',
+            'MuiAppBar-positionFixed-7',
+            'MuiAppBar-colorPrimary-13',
+            'mui-fixed',
+            'MaterialHeader-appBar-2',
+          )}
+        >
+          {/*className={classes.appBar}*/}
+          {/*{myStyles.app_bar}*/}
+          <Toolbar
+            className={classnames(
+              'MuiToolbar-root-42',
+              'MuiToolbar-regular-44',
+              'MuiToolbar-gutters-43',
+              'MaterialHeader-myToolbar-4',
+            )}
+          >
+            {/*{ gutters: classes.myToolbar }*/}
             <IconButton color="inherit" aria-label="Open drawer" onClick={this.props.handleDrawerToggle}>
               <MenuIcon />
             </IconButton>
-            <Typography variant="h6" color="inherit" noWrap classes={{ root: classes.headerTitle }}>
-              Mini variant drawer
+            <Typography
+               variant="h6"
+               color="inherit"
+               noWrap
+               classes={{root: myStyles["MaterialHeader-headerTitle-5"]}}
+            >/*classnames(
+                 'MuiTypography-root-64',
+                 'MaterialHeader-headerTitle-5',
+                 'MuiTypography-h6-81',
+                 'MuiTypography-colorInherit-93',
+                 'MuiTypography-noWrap-90',
+               )*//*classes={{ root: classes.headerTitle }}*/
+            Mini variant drawer
             </Typography>
           </Toolbar>
         </AppBar>
@@ -57,6 +89,7 @@ class MaterialHeader extends React.Component<Props> {
     );
   }
 }
+
 const WithStyles = withStyles(styles as any, { withTheme: true })(MaterialHeader);
 
 export const MiniHeader = WithStyles;
