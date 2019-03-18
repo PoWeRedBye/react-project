3bbd22fdacde1193dde5713d4bcb2e66691536a4
Maxim_Ozarovskiy <maxim.ozarovskiy@gmail.com>
2019-03-18 17:20:40 +0200

    add button send to input field(need to fix later)

--- src/components/search_input/SearchInput.tsx
+++ src/components/search_input/SearchInput.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 //Material
+import IconButton from '@material-ui/core/IconButton';
 import TextField from '@material-ui/core/TextField';
+import { InputAdornment } from '@material-ui/core';
+import { Send } from '@material-ui/icons';
 
 import { Props } from './types';
 import styles from './SearchInput.module.scss';
+import send  from '../../icons/done.svg';
 
 export class SearchInput extends React.Component<Props> {
   //RENDER
@@ -16,6 +20,15 @@ export class SearchInput extends React.Component<Props> {
         classes={{ root: styles.search_input }}
         InputLabelProps={{
           classes: { outlined: styles.label },
+        }}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton aria-label="Toggle password visibility">
+                <Send/>
+              </IconButton>
+            </InputAdornment>
+          ),
         }}
       />
     );
