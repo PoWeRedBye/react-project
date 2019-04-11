67a3042ddd2b7b32c68d21375d9774ebf62a7332
Poliakov Maksym <polmax09@gmail.com>
2019-04-11 20:37:05 +0300

    Replace class component with functional component

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -1,29 +1,20 @@
 import React from 'react';
 
 import { Snackbar } from 'material';
-
 import { Props } from './types';
 
-export class Toast extends React.Component<Props> {
-  // RENDER
-
-  render() {
-    const { open, message, handleClose } = this.props;
-
-    return (
-      <Snackbar
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        autoHideDuration={6000}
-        ContentProps={{
-          'aria-describedby': 'message-id',
-        }}
-        open={open}
-        message={<span>{message}</span>}
-        onClose={handleClose}
-      />
-    );
-  }
-}
+export const Toast: React.FunctionComponent<Props> = props => (
+  <Snackbar
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'left',
+    }}
+    autoHideDuration={6000}
+    ContentProps={{
+      'aria-describedby': 'message-id',
+    }}
+    open={props.open}
+    message={<span>{props.message}</span>}
+    onClose={props.handleClose}
+  />
+);
