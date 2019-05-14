c43f1cf75bd15ed09b6992c500fc7c2f06a8935f
Maxim_Ozarovskiy <maxim.ozarovskiy@gmail.com>
2019-05-14 14:50:46 +0300

    add act template

--- src/temprorary/ActTemplateHtmlPage/index.tsx
+++ src/temprorary/ActTemplateHtmlPage/index.tsx
@@ -6,10 +6,17 @@ const number = {
   date: '20 лютого 2019',
 };
 const client = {
-  name_caps: 'ЖОРНИК МАКСИМ ОЛЕКСІЙОВИЧ',
-  name_with_initials: 'M.О. Жорник',
-  device_name: 'bizhub С5501',
-  device_sn: 'A0U1021000270',
+  act_number: '10',
+  act_date: '24 квітня 2019',
+  organization_structure: 'ФІЗИЧНА ОСОБА - ПІДПРИЕМЕЦЬ',
+  name_caps: 'КОЧНЕВ КОНСТЯНТИН В\'ЯЧЕСЛАВОВИЧ',
+  name_with_initials: 'К.В. Кочнев',
+  adress:'61103, м.Харків, пр. Науки, 77, кв. 102',
+  edrpo_drfo:'2465420673',
+  client_rr:'26002578866900 в ПАТ"УкрСиббанк"',
+  mfo:'351005',
+  device_name: 'bizhub С6501',
+  device_sn: 'A0U2021000134',
   bw_price_name: 'За один чорно-білий відбиток',
   bw_price: 0.62,
   color_price_name: 'За один повнокольоровий відбиток',
@@ -27,15 +34,15 @@ export class ActTemplateHtmlPage extends React.Component {
         {/*шапка*/}
         <div style={{ display: 'flex', flexDirection: 'row', marginTop: '25px' }}>
           <div
-            style={{ display: 'flex', flex: 0.8, justifyContent: 'center', paddingLeft: '100px', }}>
-            Act number
+            style={{ display: 'flex', flex: 0.8, justifyContent: 'center', paddingLeft: '100px', fontWeight: 800}}>
+            АКТ №: {client.act_number}
           </div>
-          <div style={{ display: 'flex', flex: 0.2, justifyContent: 'center', fontSize: 'smaller' }}>
-            25 квітня 2019 р.
+          <div style={{ display: 'flex', flex: 0.2, justifyContent: 'center', fontSize: 'smaller', fontWeight: 800}}>
+            {client.act_date}
           </div>
         </div>
         <div style={{ display: 'flex', justifyContent: 'center', fontSize: 'small', fontWeight: 800 }}>
-          виконаних робіт за договором № ${number.some}` C + M від ${number.date}
+          виконаних робіт за договором №{number.some} C + M від {number.date}
         </div>
         <div style={{ display: 'flex', justifyContent: 'center', fontSize: 'small', fontWeight: 800 }}>
           з комплексного обслуговування виробництва копій або відбитків
@@ -55,7 +62,7 @@ export class ActTemplateHtmlPage extends React.Component {
         <div
           style={{ border: '1px solid black', borderTopWidth: 1, borderBottomWidth: 0, marginTop: '15px', }}>
           <div style={{ border: '1px solid black', height: '3px' }} />
-          <div style={{ display: 'flex', flexDirection: 'row', minHeight: '15mm' }}>
+          <div style={{ display: 'flex', flexDirection: 'row', minHeight: '15mm', backgroundColor: '#89d289' }}>
             <div style={{ textAlign: 'center', border: '1px solid black', width: '8mm', fontWeight: 800, fontSize: 'small', }}>№</div>
             <div style={{ textAlign: 'center', border: '1px solid black', width: '128mm', fontWeight: 800, fontSize: 'small', }}>Найменування</div>
             <div style={{ textAlign: 'center', border: '1px solid black', width: '26mm', fontWeight: 800, fontSize: 'small', }}>Ціна копії, грн. згідно договору</div>
@@ -64,14 +71,14 @@ export class ActTemplateHtmlPage extends React.Component {
         </div>
         <div style={{ border: '1px solid black', borderTopWidth: 0 }}>
           <div style={{ display: 'flex', flexDirection: 'row', wordBreak: 'break-word', minHeight: '5mm', }}>
-            <div style={{ textAlign: 'center', border: '1px solid black', width: '8mm', fontWeight: 800, fontSize: 'small', }}>{1}</div>
-            <div style={{ textAlign: 'start', border: '1px solid black', width: '128mm', fontWeight: 800, fontSize: 'small', }}>{client.bw_price_name}</div>
-            <div style={{ textAlign: 'center', border: '1px solid black', width: '26mm', fontWeight: 800, fontSize: 'small', }}>{client.bw_price}</div>
+            <div style={{ textAlign: 'center', border: '1px solid black', width: '8mm', fontWeight: 400, fontSize: 'small', }}>{1}</div>
+            <div style={{ textAlign: 'start', border: '1px solid black', width: '128mm', fontWeight: 400, fontSize: 'small', }}>{client.bw_price_name}</div>
+            <div style={{ textAlign: 'center', border: '1px solid black', width: '26mm', fontWeight: 400, fontSize: 'small', }}>{client.bw_price}</div>
           </div>
           <div style={{ display: 'flex', flexDirection: 'row', wordBreak: 'break-word', minHeight: '5mm', }}>
-            <div style={{ textAlign: 'center', border: '1px solid black', width: '8mm', fontWeight: 800, fontSize: 'small', }}>{2}</div>
-            <div style={{ textAlign: 'start', border: '1px solid black', width: '128mm', fontWeight: 800, fontSize: 'small', }}>{client.color_price_name}</div>
-            <div style={{ textAlign: 'center', border: '1px solid black', width: '26mm', fontWeight: 800, fontSize: 'small', }}>{client.color_price}</div>
+            <div style={{ textAlign: 'center', border: '1px solid black', width: '8mm', fontWeight: 400, fontSize: 'small', }}>{2}</div>
+            <div style={{ textAlign: 'start', border: '1px solid black', width: '128mm', fontWeight: 400, fontSize: 'small', }}>{client.color_price_name}</div>
+            <div style={{ textAlign: 'center', border: '1px solid black', width: '26mm', fontWeight: 400, fontSize: 'small', }}>{client.color_price}</div>
           </div>
           <div style={{ border: '1px solid black', height: '3px' }} />
         </div>
@@ -79,7 +86,7 @@ export class ActTemplateHtmlPage extends React.Component {
         <div style={{ display: 'flex', justifyContent: 'center', marginTop: '15px', fontWeight: 800, fontSize: 'small', }}>2. Показники лічільників</div>
         <div style={{ border: '1px solid black', borderTopWidth: 1, borderBottomWidth: 0, marginTop: '15px', }}>
           <div style={{ border: '1px solid black', height: '3px' }} />
-          <div style={{ display: 'flex', flexDirection: 'row', minHeight: '15mm' }}>
+          <div style={{ display: 'flex', flexDirection: 'row', minHeight: '15mm', backgroundColor: '#89d289' }}>
             <div style={{ textAlign: 'center', border: '1px solid black', width: '8mm', fontWeight: 800, fontSize: 'small', }}>№</div>
             <div style={{ textAlign: 'center', border: '1px solid black', width: '62mm', fontWeight: 800, fontSize: 'small', }}>Найменування</div>
             <div style={{ textAlign: 'center', border: '1px solid black', width: '22mm', fontWeight: 800, fontSize: 'small', }}>Попередні показники лічільників</div>
@@ -91,20 +98,20 @@ export class ActTemplateHtmlPage extends React.Component {
         </div>
         <div style={{ border: '1px solid black', borderTopWidth: 0 }}>
           <div style={{ display: 'flex', flexDirection: 'row', wordBreak: 'break-word', minHeight: '5mm', }}>
-            <div style={{ textAlign: 'center', border: '1px solid black', width: '8mm', fontWeight: 800, fontSize: 'small', }}>{1}</div>
-            <div style={{ textAlign: 'start', border: '1px solid black', width: '62mm', fontWeight: 800, fontSize: 'small', }}>Послуги з комплексного обслуговування копій: чорно-білі відбитки</div>
-            <div style={{ textAlign: 'center', border: '1px solid black', width: '22mm', fontWeight: 800, fontSize: 'small', }}>{client.bw_prev_counter.toLocaleString()}</div>
-            <div style={{ textAlign: 'center', border: '1px solid black', width: '22mm', fontWeight: 800, fontSize: 'small', }}>{client.bw_current_counter.toLocaleString()}</div>
-            <div style={{ textAlign: 'center', border: '1px solid black', width: '23mm', fontWeight: 800, fontSize: 'small', }}>{(client.bw_current_counter - client.bw_prev_counter).toLocaleString()}</div>
-            <div style={{ textAlign: 'center', border: '1px solid black', width: '26mm', fontWeight: 800, fontSize: 'small', }}>{((client.bw_current_counter - client.bw_prev_counter) * client.bw_price).toFixed(2).toLocaleString()}</div>
+            <div style={{ textAlign: 'center', border: '1px solid black', width: '8mm', fontWeight: 400, fontSize: 'small', }}>{1}</div>
+            <div style={{ textAlign: 'start', border: '1px solid black', width: '62mm', fontWeight: 400, fontSize: 'small', }}>Послуги з комплексного обслуговування копій: чорно-білі відбитки</div>
+            <div style={{ textAlign: 'center', border: '1px solid black', width: '22mm', fontWeight: 400, fontSize: 'small', }}>{client.bw_prev_counter.toLocaleString()}</div>
+            <div style={{ textAlign: 'center', border: '1px solid black', width: '22mm', fontWeight: 400, fontSize: 'small', }}>{client.bw_current_counter.toLocaleString()}</div>
+            <div style={{ textAlign: 'center', border: '1px solid black', width: '23mm', fontWeight: 400, fontSize: 'small', }}>{(client.bw_current_counter - client.bw_prev_counter).toLocaleString()}</div>
+            <div style={{ textAlign: 'center', border: '1px solid black', width: '26mm', fontWeight: 400, fontSize: 'small', }}>{((client.bw_current_counter - client.bw_prev_counter) * client.bw_price).toFixed(2).toLocaleString()}</div>
           </div>
           <div style={{ display: 'flex', flexDirection: 'row', wordBreak: 'break-word', minHeight: '5mm', }}>
-            <div style={{ textAlign: 'center', border: '1px solid black', width: '8mm', fontWeight: 800, fontSize: 'small', }}>{2}</div>
-            <div style={{ textAlign: 'start', border: '1px solid black', width: '62mm', fontWeight: 800, fontSize: 'small', }}>Послуги з комплексного обслуговування копій: повнокольорові відбитки</div>
-            <div style={{ textAlign: 'center', border: '1px solid black', width: '22mm', fontWeight: 800, fontSize: 'small', }}>{client.color_prev_counter.toLocaleString()}</div>
-            <div style={{ textAlign: 'center', border: '1px solid black', width: '22mm', fontWeight: 800, fontSize: 'small', }}>{client.color_current_counter.toLocaleString()}</div>
-            <div style={{ textAlign: 'center', border: '1px solid black', width: '23mm', fontWeight: 800, fontSize: 'small', }}>{(client.color_current_counter - client.color_prev_counter).toLocaleString()}</div>
-            <div style={{ textAlign: 'center', border: '1px solid black', width: '26mm', fontWeight: 800, fontSize: 'small', }}>{((client.color_current_counter - client.color_prev_counter) * client.color_price).toFixed(2).toLocaleString()}</div>
+            <div style={{ textAlign: 'center', border: '1px solid black', width: '8mm', fontWeight: 400, fontSize: 'small', }}>{2}</div>
+            <div style={{ textAlign: 'start', border: '1px solid black', width: '62mm', fontWeight: 400, fontSize: 'small', }}>Послуги з комплексного обслуговування копій: повнокольорові відбитки</div>
+            <div style={{ textAlign: 'center', border: '1px solid black', width: '22mm', fontWeight: 400, fontSize: 'small', }}>{client.color_prev_counter.toLocaleString()}</div>
+            <div style={{ textAlign: 'center', border: '1px solid black', width: '22mm', fontWeight: 400, fontSize: 'small', }}>{client.color_current_counter.toLocaleString()}</div>
+            <div style={{ textAlign: 'center', border: '1px solid black', width: '23mm', fontWeight: 400, fontSize: 'small', }}>{(client.color_current_counter - client.color_prev_counter).toLocaleString()}</div>
+            <div style={{ textAlign: 'center', border: '1px solid black', width: '26mm', fontWeight: 400, fontSize: 'small', }}>{((client.color_current_counter - client.color_prev_counter) * client.color_price).toFixed(2).toLocaleString()}</div>
           </div>
           <div style={{ border: '1px solid black', height: '3px' }} />
         </div>
@@ -123,8 +130,38 @@ export class ActTemplateHtmlPage extends React.Component {
           </div>
         </div>
         {/*всего прописью*/}
-        <div>
-
+        <div style={{ display: 'flex', flexDirection: 'column', paddingTop: '10px' }}>
+          <div style={{fontWeight: 800, fontSize: 'small'}} >Всього на сумму:</div>
+          <div style={{fontWeight: 800, fontSize: 'small'}}>Сумма прописью</div>
+          <div style={{fontWeight: 800, fontSize: 'small'}}>ПДВ: не передбачено</div>
+          <div style={{marginTop: '5mm',fontSize: 'small'}}>Замовник претензій по об'ему, якості та строкам виконання робіт (надання послуг) не має.</div>
+        </div>
+        {/*печати + банковская информация сторон*/}
+        <div style={{display: 'flex', flex: 1, flexDirection: 'row', marginTop: '5mm'}}>
+          <div style={{display: 'flex',flexDirection: 'column', flex: 0.5, paddingRight: '15px'}}>
+            <div style={{fontWeight: 800, fontSize: 'small'}}>Від Віконавця</div>
+            <div style={{fontWeight: 800, fontSize: 'small', marginTop: '25px'}}>______________________________________</div>
+            <div style={{fontWeight: 400, fontSize: 'smaller', textAlign: 'end'}}>М.І. Озаровський</div>
+            <div style={{fontWeight: 400, fontSize: 'smaller', textAlign: 'start'}}>м.п.</div>
+            <div style={{fontWeight: 800, fontSize: 'small', textAlign: 'center'}}>ФІЗИЧНА ОСОБА - ПІДПРИЕМЕЦЬ</div>
+            <div style={{fontWeight: 800, fontSize: 'small', textAlign: 'center'}}>ОЗАРОВСЬКИЙ МАКСИМ ІГОРОВИЧ</div>
+            <div style={{fontWeight: 400, fontSize: 'smaller', textAlign: 'start', marginTop: '20px'}}>61145, м.Харків, вул Новгородська, 4, кв.92</div>
+            <div style={{fontWeight: 400, fontSize: 'smaller', textAlign: 'start',marginTop: '20px'}}>код ЄДРПО/ДРФО: 3253008234</div>
+            <div style={{fontWeight: 400, fontSize: 'smaller', textAlign: 'start'}}>Р/р: 26003052119396 в АТ КБ ПриватБанк,</div>
+            <div style={{fontWeight: 400, fontSize: 'smaller', textAlign: 'start', marginTop: '20px'}}>МФО: 351533</div>
+          </div>
+          <div style={{display: 'flex',flexDirection: 'column', flex: 0.5, paddingLeft: '15px'}}>
+            <div style={{fontWeight: 800, fontSize: 'small'}}>Від Замовника</div>
+            <div style={{fontWeight: 800, fontSize: 'small', marginTop: '25px'}}>______________________________________</div>
+            <div style={{fontWeight: 400, fontSize: 'smaller', textAlign: 'end'}}>{client.name_with_initials}</div>
+            <div style={{fontWeight: 400, fontSize: 'smaller', textAlign: 'start'}}>м.п.</div>
+            <div style={{fontWeight: 800, fontSize: 'small', textAlign: 'center'}}>{client.organization_structure}</div>
+            <div style={{fontWeight: 800, fontSize: 'small', textAlign: 'center'}}>{client.name_caps}</div>
+            <div style={{fontWeight: 400, fontSize: 'smaller', textAlign: 'start', marginTop: '20px'}}>{client.adress}</div>
+            <div style={{fontWeight: 400, fontSize: 'smaller', textAlign: 'start',marginTop: '20px'}}>код ЄДРПО/ДРФО: {client.edrpo_drfo}</div>
+            <div style={{fontWeight: 400, fontSize: 'smaller', textAlign: 'start'}}>Р/р: {client.client_rr},</div>
+            <div style={{fontWeight: 400, fontSize: 'smaller', textAlign: 'start', marginTop: '20px'}}>МФО: {client.mfo}</div>
+          </div>
         </div>
       </React.Fragment>
     );
